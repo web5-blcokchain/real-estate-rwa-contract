bfd77503dd7355b1ee0087d4f3abce879f5e12ba
jim <jim.zh@google.com>
2025-03-22 14:06:26 +0800

    feat:imporve RealEstateToken.sol

--- contracts/RealEstateToken.sol
+++ contracts/RealEstateToken.sol
@@ -5,16 +5,18 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "./PropertyRegistry.sol";
 
 /**
  * @title RealEstateToken
  * @dev 房产代币，代表对特定房产的所有权份额
  */
-contract RealEstateToken is Initializable, ERC20Upgradeable, AccessControlUpgradeable, UUPSUpgradeable {
+contract RealEstateToken is Initializable, ERC20Upgradeable, AccessControlUpgradeable, PausableUpgradeable, UUPSUpgradeable {
     // 角色定义
     bytes32 public constant SUPER_ADMIN_ROLE = keccak256("SUPER_ADMIN_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     
     // 房产信息
     string public propertyId;
@@ -29,12 +31,15 @@ contract RealEstateToken is Initializable, ERC20Upgradeable, AccessControlUpgrad
     // 白名单功能开关
     bool public whitelistEnabled;
     
+    // 代币总供应量上限
+    uint256 public maxSupply;
+    
     // 事件
     event WhitelistUpdated(address indexed user, bool status);
     event TransferRestrictionUpdated(bool restricted);
-    event WhitelistEnabledUpdated(bool enabled);
-    // 删除: event OperationalStatusUpdated(OperationalStatus status);
-
+    event WhitelistEnabledUpdated(bool indexed enabled);
+    event MaxSupplyUpdated(uint256 indexed oldMaxSupply, uint256 indexed newMaxSupply);
+    
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
@@ -52,6 +57,7 @@ contract RealEstateToken is Initializable, ERC20Upgradeable, AccessControlUpgrad
     ) public initializer {
         __ERC20_init(_name, _symbol);
         __AccessControl_init();
+        __Pausable_init();
         __UUPSUpgradeable_init();
         
         propertyId = _propertyId;
@@ -60,10 +66,12 @@ contract RealEstateToken is Initializable, ERC20Upgradeable, AccessControlUpgrad
         propertyRegistry = PropertyRegistry(_propertyRegistry);
         transferRestricted = true;
         whitelistEnabled = false; // 默认不启用白名单功能
+        maxSupply = 1000000 * 10**decimals(); // 默认设置为100万代币
         
         _setupRole(DEFAULT_ADMIN_ROLE, _admin);
         _setupRole(SUPER_ADMIN_ROLE, _admin);
         _setupRole(MINTER_ROLE, _admin);
+        _setupRole(PAUSER_ROLE, _admin);
         
         // 自动将管理员添加到白名单
         whitelist[_admin] = true;
@@ -74,7 +82,8 @@ contract RealEstateToken is Initializable, ERC20Upgradeable, AccessControlUpgrad
      * @param to 接收者地址
      * @param amount 代币数量
      */
-    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) whenNotPaused {
+        require(totalSupply() + amount <= maxSupply, "Exceeds maximum supply");
         _mint(to, amount);
     }
     
@@ -82,8 +91,33 @@ contract RealEstateToken is Initializable, ERC20Upgradeable, AccessControlUpgrad
      * @dev 销毁代币
      * @param amount 代币数量
      */
-    function burn(uint256 amount) external {
+    function burn(uint256 amount) external whenNotPaused {
         _burn(msg.sender, amount);
+    }
+    
+    /**
+     * @dev 设置最大供应量
+     * @param _maxSupply 新的最大供应量
+     */
+    function setMaxSupply(uint256 _maxSupply) external onlyRole(SUPER_ADMIN_ROLE) {
+        require(_maxSupply >= totalSupply(), "New max supply must be >= current total supply");
+        uint256 oldMaxSupply = maxSupply;
+        maxSupply = _maxSupply;
+        emit MaxSupplyUpdated(oldMaxSupply, _maxSupply);
+    }
+    
+    /**
+     * @dev 暂停所有代币操作
+     */
+    function pause() external onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+    
+    /**
+     * @dev 恢复所有代币操作
+     */
+    function unpause() external onlyRole(PAUSER_ROLE) {
+        _unpause();
     }
     
     /**
@@ -138,7 +172,7 @@ contract RealEstateToken is Initializable, ERC20Upgradeable, AccessControlUpgrad
     /**
      * @dev 重写transfer函数，添加白名单检查
      */
-    function transfer(address to, uint256 amount) public override returns (bool) {
+    function transfer(address to, uint256 amount) public override whenNotPaused returns (bool) {
         _checkTransferRestrictions(msg.sender, to);
         return super.transfer(to, amount);
     }
@@ -146,7 +180,7 @@ contract RealEstateToken is Initializable, ERC20Upgradeable, AccessControlUpgrad
     /**
      * @dev 重写transferFrom函数，添加白名单检查
      */
-    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
+    function transferFrom(address from, address to, uint256 amount) public override whenNotPaused returns (bool) {
         _checkTransferRestrictions(from, to);
         return super.transferFrom(from, to, amount);
     }
@@ -222,7 +256,7 @@ contract RealEstateToken is Initializable, ERC20Upgradeable, AccessControlUpgrad
      * @param recipients 接收者地址数组
      * @param amounts 代币数量数组
      */
-    function batchTransfer(address[] calldata recipients, uint256[] calldata amounts) external {
+    function batchTransfer(address[] calldata recipients, uint256[] calldata amounts) external whenNotPaused {
         require(recipients.length == amounts.length, "Array lengths must match");
         
         for (uint256 i = 0; i < recipients.length; i++) {
