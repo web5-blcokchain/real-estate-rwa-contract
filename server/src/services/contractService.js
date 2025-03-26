4a126b791d8e84e2d641e7ed499b0f8b3b839753
jim <jim.zh@google.com>
2025-03-26 09:32:46 +0800

    fix:bug

--- server/src/services/contractService.js
+++ server/src/services/contractService.js
@@ -1,5 +1,15 @@
-const { contractService } = require('../../../shared/utils/contractService');
-const { logger } = require('../utils/logger');
+const TokenFactoryService = require('../../../shared/services/tokenFactoryService');
+const PropertyRegistryService = require('../../../shared/services/propertyRegistryService');
+const RealEstateTokenService = require('../../../shared/services/realEstateTokenService');
+const logger = require('../utils/logger');
 
-// 导出共享的合约服务
-module.exports = contractService; 
+// 创建服务实例
+const tokenFactoryService = new TokenFactoryService();
+const propertyRegistryService = new PropertyRegistryService();
+
+// 导出服务实例
+module.exports = {
+  tokenFactory: tokenFactoryService,
+  propertyRegistry: propertyRegistryService,
+  getToken: (tokenAddress) => new RealEstateTokenService(tokenAddress)
+}; 
